8f5b5616c5c1b6abb01577857cd3198c695f63ad
ararat1393 <54005752+ararat1393@users.noreply.github.com>
2020-05-06 18:46:52 +0400

    update code

--- model.php
+++ model.php
@@ -1,5 +1,15 @@
 <?php
 
+// ** MySQL settings - You can get this info from your web host ** //
+/** The name of the database for WordPress */
+define( 'DB_NAME', 'vue' );
+/** MySQL database username */
+define( 'DB_USER', 'root' );
+/** MySQL database password */
+define( 'DB_PASSWORD', '' );
+/** MySQL hostname */
+define( 'DB_HOST', 'localhost' );
+/** Database Charset to use in creating database tables. */
 
 class BaseModel
 {
@@ -15,12 +25,17 @@ class BaseModel
     const SQL_VALUES = 'VALUES';
     const SQL_AND = 'AND';
     const SQL_OR = 'OR';
+    const SQL_ASC = 'ASC';
+    const SQL_DESC = 'DESC';
+    const SQL_BETWEEN = 'BETWEEN';
+    const SQL_NOT_BETWEEN = 'NOT BETWEEN';
+    const SQL_SET = 'SET';
 
     private $model;
 
-    protected $table;
+    private static $table;
     private $selectedColumns;
-    private $query;
+    private static $query;
     private $_where = [];
     private $limit;
     private $orderBy;
@@ -32,29 +47,27 @@ class BaseModel
     private $condition;
 
     /**
-     * create connection
-     * @param $host
-     * @param $user
-     * @param $password
-     * @param null $name database name
+     * BaseModel constructor.
+     * @throws Exception
      */
-    public function __construct( $host , $user , $password , $name )
+    public function __construct( $table = null )
     {
-        $this->connection($host,$user,$password,$name);
+        if( $table ) self::$table = $table;
+        $this->connection(DB_HOST,DB_USER,DB_PASSWORD,DB_NAME);
     }
 
     /**
      * @param $host
-     * @param $user
+     * @param $result
      * @param $password
      * @param $name
      * @return false|resource
      * @throws \Exception
      */
-    private function connection( $host , $user , $password , $name )
+    private function connection( $host , $result , $password , $name )
     {
         if(!is_resource($this->model) || empty($this->model)){
-            if( $this->model = mysqli_connect($host, $user, $password,$name)){
+            if( $this->model = mysqli_connect($host, $result, $password,$name)){
                 $this->model->set_charset('utf8');
             }else{
                 throw new \Exception('Could not connect to MySQL database.');
@@ -69,8 +82,7 @@ class BaseModel
      */
     public function table($tableName)
     {
-        $this->table = $tableName;
-        return $this;
+        return new BaseModel($tableName);
     }
 
     /**
@@ -79,9 +91,6 @@ class BaseModel
      */
     public function select(Array $selectedColumns = [])
     {
-        if( $this->action )
-            throw new \Exception("SQL action is already installed");
-
         $this->action = self::SQL_ACTION_SELECT;
         if(!empty($selectedColumns)){
             $this->selectedColumns = implode(',',$selectedColumns);
@@ -94,15 +103,15 @@ class BaseModel
     /**
      * @param String $key
      * @param String $operator
-     * @param String $value
+     * @param $value
      * @return $this
      */
-    public function where(String $key,String $operator, String $value)
+    public function where(String $key,String $operator, $value)
     {
        if( !empty($this->condition) ){
-           $this->condition .= " AND ";
+           $this->condition .= sprintf(" %s " , self::SQL_AND);
        }
-       $this->condition .= "(`$key` $operator $value)";
+       $this->condition .= sprintf(" (`%s` %s '%s') " ,$key,$operator,$value);
        return $this;
     }
 
@@ -112,9 +121,9 @@ class BaseModel
      * @return $this
      * @throws \Exception
      */
-    public function orWhere(Array $conditions, $separator = SQL_AND )
+    public function orWhere(Array $conditions, $separator = self::SQL_AND )
     {
-        return $this->_where( $conditions , $separator , SQL_OR );
+        return $this->_where( $conditions , $separator , self::SQL_OR );
     }
 
     /**
@@ -127,12 +136,52 @@ class BaseModel
         return $this->_where( $conditions );
     }
 
-    
-    private function _where(Array $conditions, $separator = SQL_AND , $operator = SQL_AND )
+    /**
+     * @param String $key
+     * @param $value_1
+     * @param $value_2
+     * @return $this
+     * @throws Exception
+     */
+    public function between(String $key,$value_1,$value_2)
     {
+        if( !empty($this->condition) ){
+            $this->condition .= sprintf(" %s " , self::SQL_AND);
+        }
+        $this->condition .= sprintf(" ( `%s` %s '%s' %s '%s') ",$key,self::SQL_BETWEEN,$value_1,self::SQL_AND,$value_2);
+        return $this;
+    }
+
+    /**
+     * @param String $key
+     * @param $value_1
+     * @param $value_2
+     * @return $this
+     * @throws Exception
+     */
+    public function notBetween( String $key ,$value_1,$value_2 )
+    {
+        if( !empty($this->condition) ){
+            $this->condition .= sprintf(" %s " , self::SQL_AND);
+        }
+        $this->condition .= sprintf(" ( `%s` %s '%s' %s '%s') ",$key,self::SQL_NOT_BETWEEN,$value_1,self::SQL_AND,$value_2);
+        return $this;
+    }
+
+    /**
+     * @param array $conditions
+     * @param $separator
+     * @param $operator
+     * @return $this
+     * @throws Exception
+     */
+    private function _where(Array $conditions, $separator = self::SQL_AND , $operator = self::SQL_AND )
+    {
+
         if( !empty($this->condition) ){
             $this->condition .=  sprintf(" %s ", $operator);
         }
+        $this->_where = [];
         foreach ($conditions as $condition ){
             list($operator,$key,$values) = $condition;
             if( in_array(strtolower($operator),['in','not in'])){
@@ -144,17 +193,17 @@ class BaseModel
                         $value .= sprintf("'%s',", $item);
                     }
                     $value = substr($value, 0, -1);
-                    $this->_where[] = "`$key` $operator ( $value )";
+                    $this->_where[] = sprintf("`%s` %s (%s)",$key,$operator,$value);
                 }
             }else{
                 if( !is_string($values)){
                     throw new \Exception('value must be an string');
                 }else{
-                    $this->_where[] = "`$key` $operator $value ";
+                    $this->_where[] = sprintf("`%s` %s '%s'",$key,$operator,$values);
                 }
             }
         }
-        $this->condition .= " ( ".implode($separator,$this->andWhere) . " ) ";
+        $this->condition .= " ( ".implode($separator,$this->_where) . " ) ";
         return $this;
     }
 
@@ -162,12 +211,12 @@ class BaseModel
      * @param String $key
      * @param String|null $order
      */
-    public function orderBy(String $key,String $order = null)
+    public function orderBy(String $key,String $order = self::SQL_ASC)
     {
-        if( !in_array(strtolower($order),['asc','desc']) ){
-            $order = 'asc';
+        if( !in_array(strtoupper($order),[self::SQL_ASC,self::SQL_DESC]) ){
+            $order = self::SQL_ASC;
         }
-        $this->orderBy = self::SQL_ORDER_BY ." `$key` $order";
+        $this->orderBy = sprintf(" %s `%s` %s ",self::SQL_ORDER_BY,$key,$order);
         return $this;
     }
 
@@ -177,7 +226,7 @@ class BaseModel
      */
     public function groupBy(String $key)
     {
-        $this->groupBy = self::SQL_GROUP_BY ." `$key` ";
+        $this->groupBy = sprintf(" %s `%s` ",self::SQL_GROUP_BY ,$key );
         return $this;
     }
 
@@ -191,7 +240,7 @@ class BaseModel
     {
         if( !empty($this->limit) )
             throw new \Exception('Limit already installed');
-        $this->limit = self::SQL_LIMIT ." ". $offset.",".$limit;
+        $this->limit = sprintf(" %s %u,%u ",self::SQL_LIMIT, $offset , $limit );
         return $this;
     }
     /**
@@ -200,13 +249,13 @@ class BaseModel
      */
     public function one()
     {
-        if ($result = $this->model->query($this->querySQL())) {
-            if ($result->num_rows > 0) {
+        if ($result = $this->model->query($this->_select())) {
+            if ($result->num_rows > 0)
                 return $result->fetch_object();
-            }
-        }else{
-            throw new \Exception("Error description: " . $this->model->error);
         }
+        else
+            throw new \Exception("Error description: " . $this->model->error);
+
         return new \stdClass();
     }
 
@@ -216,56 +265,76 @@ class BaseModel
      */
     public function all()
     {
-        if( $result = $this->model->query($this->querySQL()) ){
+        if( $result = $this->model->query($this->_select()) )
             return $result->fetch_all(MYSQLI_ASSOC);
-        }
-        throw new \Exception("Error description: " . $this->model->error);
+        else
+            throw new \Exception("Error description: " . $this->model->error);
+        return [];
     }
 
+    /**
+     * @param $values
+     * @return mixed|stdClass|null
+     * @throws Exception
+     */
     public function insert($values)
     {
         if( $this->action )
             throw new \Exception("SQL action is already installed");
+        if(empty($values))
+            throw new \Exception("Value can't be empty");
 
         $this->action = self::SQL_ACTION_INSERT;
-    }
 
-    public function update($condition = [],$sets = [])
-    {
-        if( $this->action )
-            throw new \Exception("SQL action is already installed");
+        foreach($values as $column => $value){
+            $this->fields .= sprintf("`%s`,", $column);
+            $this->values .= sprintf("'%s',", addslashes($value));
+        }
+        $this->fields = substr($this->fields, 0, -1);
+        $this->values = substr($this->values, 0, -1);
 
-        $this->action = self::SQL_ACTION_UPDATE;
-    }
-
-    public function delete($condition = [])
-    {
-        if( $this->action )
-            throw new \Exception("SQL action is already installed");
-
-        $this->action = self::SQL_ACTION_DELETE;
+        if( $this->model->query( $this->_insert() )){
+            return $this->select()
+                ->where('id','=',$this->model->insert_id)
+                ->one();
+        }
+        return true;
     }
 
     /**
-     * @return string
+     * @param array $sets
+     * @return bool
+     * @throws Exception
      */
-    private function querySQL()
+    public function update( Array $sets )
     {
-        switch ($this->action) {
-            case self::SQL_ACTION_SELECT:
-                $this->query = $this->_select();
-                break;
-            case self::SQL_ACTION_INSERT:
-                $this->query = $this->_insert();
-                break;
-            case self::SQL_ACTION_DELETE:
-                $this->query = $this->_delete();
-                break;
-            case self::SQL_ACTION_UPDATE:
-                $this->query = $this->_update();
-                break;
+        if( $this->action )
+            throw new \Exception("SQL action is already installed");
+        $this->action = self::SQL_ACTION_UPDATE;
+
+        foreach($sets as $column => $value){
+            $this->sets .= sprintf(" `%s`='%s',", $column , addslashes($value) );
         }
-        return $this->query;
+        $this->sets = substr($this->sets, 0, -1);
+
+        if( $this->model->query( $this->_update())) return true;
+
+        return false;
+    }
+
+    /**
+     * @return bool
+     * @throws Exception
+     */
+    public function delete()
+    {
+        if( $this->action )
+            throw new \Exception("SQL action is already installed");
+        $this->action = self::SQL_ACTION_DELETE;
+
+        if( $this->model->query( $this->_delete())) return true;
+
+        return false;
     }
 
     /**
@@ -273,20 +342,21 @@ class BaseModel
      */
     private function _select()
     {
-        $query = self::SQL_ACTION_SELECT. ' '. $this->selectedColumns .' '.self::SQL_FROM.' '.$this->table.' ';
+        self::$query = sprintf(" %s %s %s %s ",self::SQL_ACTION_SELECT,$this->selectedColumns,self::SQL_FROM,self::$table);
+
         if( $this->condition ){
-            $query .= self::SQL_WHERE.' '.$this->condition.' ';
+            self::$query .= sprintf(" %s %s ",self::SQL_WHERE , $this->condition );
         }
         if( $this->groupBy ) {
-            $query .= $this->groupBy.' ';
+            self::$query .= sprintf(" %s ",$this->groupBy );
         }
         if( $this->orderBy ) {
-            $query .= $this->orderBy.' ';
+            self::$query .= sprintf(" %s ",$this->orderBy );
         }
         if( $this->limit ) {
-            $query .= $this->limit.' ';
+            self::$query .= sprintf(" %s ",$this->limit );
         }
-        return $query;
+        return self::$query;
     }
 
     /**
@@ -294,9 +364,7 @@ class BaseModel
      */
     public function _insert()
     {
-        $query = self::SQL_ACTION_INSERT .' %s (%s) '.self::SQL_VALUES.' (%s)';
-        $query = sprintf($query, $this->table, $this->fields, $this->values);
-        return $query;
+        return sprintf(" %s %s (%s) %s (%s) ",self::SQL_ACTION_INSERT , self::$table , $this->fields ,self::SQL_VALUES,$this->values);
     }
 
     /**
@@ -304,11 +372,11 @@ class BaseModel
      */
     public function  _delete()
     {
-        $query = self::SQL_ACTION_DELETE. ' '. $this->selectedColumns .' '.self::SQL_FROM.' '.$this->table.' ';
+        self::$query = sprintf(" %s  %s %s ",self::SQL_ACTION_DELETE,self::SQL_FROM,self::$table);
         if( $this->condition ){
-            $query .= self::SQL_WHERE.' '.$this->condition;
+            self::$query .= sprintf(" %s %s ",self::SQL_WHERE,$this->condition);
         }
-        return $query;
+        return self::$query;
     }
 
     /**
@@ -316,29 +384,70 @@ class BaseModel
      */
     public function _update()
     {
-        $query = self::SQL_ACTION_UPDATE.' '.$this->table.' ';
+        self::$query = sprintf(" %s %s " ,self::SQL_ACTION_UPDATE , self::$table );
         if( $this->sets ){
-            $query .= self::SQL_SET.' '.$this->sets.' ';
+            self::$query .= sprintf(" %s %s ",self::SQL_SET ,$this->sets );
         }
         if( $this->condition ){
-            $query .= self::SQL_WHERE.' '.$this->condition.' ';
+            self::$query .= sprintf(" %s %s ",self::SQL_WHERE,$this->condition );
         }
-        return $query;
+        return self::$query;
     }
 
-    public function sql()
+    /**
+     * @return string
+     */
+    public function query()
     {
-        return $this->querySQL();
+        switch ($this->action) {
+            case self::SQL_ACTION_SELECT:
+                self::$query = $this->_select();
+                break;
+            case self::SQL_ACTION_INSERT:
+                self::$query = $this->_insert();
+                break;
+            case self::SQL_ACTION_DELETE:
+                self::$query = $this->_delete();
+                break;
+            case self::SQL_ACTION_UPDATE:
+                self::$query = $this->_update();
+                break;
+        }
+        return self::$query;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function error()
+    {
+        return  $this->model->error;
     }
 }
-$db = new BaseModel('localhost','root','','db');
-$user = $db->table('users')
+$db = new BaseModel();
+$result1 = $db->table('users')
     ->select(['name','email as E_Mail'])
-    ->where('id','=',11)
+    ->where('id','!=',18)
     ->andWhere([
         ['in','name',['Jone','Doe','Admin']]
     ])
+    ->orWhere([
+        ['like','email','ararat.martirosyan13@gmail.com']
+    ])
+    ->between('created_at','2020-05-01 14:58:51','2020-05-02 14:58:51')
+    ->notBetween('created_at','2020-05-01 14:58:51','2020-05-02 14:58:51')
     ->limit(14)
     ->orderBy('id','DESC')
     ->one();
-var_dump($user);die;
+var_dump($db->error());
+var_dump($result1);
+$result2 = $db->table('users')->insert(['name'=>'test','email'=>'test@gmail.com','password'=>'test']);
+var_dump($db->error());
+var_dump($result2);
+$result3 = $db->table('users')->where('name','=','test8')->delete();
+var_dump($db->error());
+var_dump($result3);
+$result4 = $db->table('users')->where('name','=','test8')->update(['name' => 'test9']);
+var_dump($db->error());
+var_dump($result4);die;
+
